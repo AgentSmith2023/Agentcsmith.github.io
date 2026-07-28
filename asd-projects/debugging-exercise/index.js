a1cfd58c4ed4c5b6f83165314be5314267b72a4c
AgentSmith2023 <148362957+AgentSmith2023@users.noreply.github.com>
2026-07-28 17:51:40 +0000

    updated file

--- asd-projects/debugging-exercise/index.js
+++ asd-projects/debugging-exercise/index.js
@@ -1,4 +1,4 @@
- $(document).ready(function () {
+   $(document).ready(function () {
   
 /////////////////
 // initialization
@@ -80,11 +80,11 @@ function decideSpeed() {
 
 // this generates an id for a ghost given the ghost's number
 function getId(number) {
-  return "ghost" + number; ()
+  return "ghost" + number; 
 }
 
 // this adds a ghost into the HTML
-funtion addNewGhostElement(ghost, id) {
+function addNewGhostElement(ghost, id) {
   // this creates the HTML for a new ghost element
   var $ghost = $("<img>")
     .attr("id", id)
@@ -136,10 +136,11 @@ function moveGhost(ghost) {
 // this bounces ghosts if they hit a wall
 function bounceGhost(ghost) {
   // this bounces off the left wall
-  if (ghost.x < 0{
+  if (ghost.x < 0){
     ghost.x -= ghost.speedX;
     ghost.speedX *= -1;
-  }
+  } 
+}
   // this bounces off the right wall
   else if (ghost.x > boardWidth) {
     ghost.x -= ghost.speedX;
